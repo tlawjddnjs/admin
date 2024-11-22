dd91d0420d8f77fed611138650794106a5d1fd55
tlawjddnjs <157342006+tlawjddnjs@users.noreply.github.com>
2024-11-22 20:20:42 +0900

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "my-mysql";  // MySQL 컨테이너 이름
-$username = "root";        // 데이터베이스 사용자 이름
-$password = "rkdwlgn1*";   // 데이터베이스 비밀번호
-$dbname = "main";          // 데이터베이스 이름
+$servername = "mysql";  // MySQL 컨테이너 이름
+$username = "root";     // MySQL 사용자 이름
+$password = "root";     // docker-compose.yml에서 설정한 MYSQL_ROOT_PASSWORD
+$dbname = "adm";        // docker-compose.yml에서 설정한 MYSQL_DATABASE
 
 // 데이터베이스 연결 생성
 $conn = new mysqli($servername, $username, $password, $dbname);
