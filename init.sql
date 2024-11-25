d862b740ba775560cf98ff2236c9aec068e4044a
tlawjddnjs <157342006+tlawjddnjs@users.noreply.github.com>
2024-11-25 13:30:07 +0900

    Update init.sql

--- init.sql
+++ init.sql
@@ -5,3 +5,12 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(100),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS reviews (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    content TEXT NOT NULL,
+    rating INT CHECK (rating BETWEEN 1 AND 5),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
